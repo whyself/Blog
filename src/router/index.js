78dd8ff1547472b3e45d20925d815ea7fffecb1c
whyself <1158820305@qq.com>
2025-10-25 16:59:15 +0800

    demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import Navigation from '../components/Navigation.vue'
+import MainView from '../components/MainView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/nav',
-    name: 'Navigation',
-    component: Navigation
+    name: 'Main',
+    component: MainView
   }
 ]
 
