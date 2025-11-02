13ff6a4a3b347b70743b1cd5b9c302cc0a337c2e
whyself <1158820305@qq.com>
2025-11-02 21:58:48 +0800

    修改文章test

--- src/articles/metadata.js
+++ src/articles/metadata.js
@@ -21,7 +21,7 @@ export const articlesMeta = [
 
   {
     id: 'test',
-    title: 'test5201314',
+    title: 'test520131',
     date: '2025-11-2',
     author: 'whyself',
     readTime: '1 min read',
